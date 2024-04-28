84101850ed38fba65233dbe5cff0209113956b7b
Abdelkhalek Amr <aaabdelkhalek3@gmail.com>
2024-04-28 06:08:24 +0300

    add dar and police

--- src/app.router.js
+++ src/app.router.js
@@ -2,6 +2,8 @@ import authRouter from "./Modules/auth/auth.router.js";
 import postRouter from "./Modules/posts/post.router.js";
 import adminRouter from "./Modules/admin/admin.router.js";
 import userprofileRouter from "./Modules/userProfile/userprofile.router.js";
+import policeRouter from "./Modules/police/police.router.js";
+import darRouter from "./Modules/dar/dar.router.js";
 import { globalErrorHandling } from "./utils/errorHandling.js";
 import cors from "cors";
 //import cookieParser from "cookie-parser";
@@ -35,6 +37,12 @@ export const appRouter = (app, express) => {
 
   // Admin
   app.use("/admin", adminRouter);
+
+  // Police
+  app.use("/police", policeRouter);
+
+  // Dat
+  app.use("/dar", darRouter);
 
   app.use("*", (req, res, next) => {
     return res.json({ Message: "In-valid routing" });
