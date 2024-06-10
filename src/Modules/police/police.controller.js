33f97a4192174b3640c066b6ae70fad4ea0b6248
Abdelkhalek Amr <aaabdelkhalek3@gmail.com>
2024-06-10 03:04:25 +0300

    get all dars

--- src/Modules/police/police.controller.js
+++ src/Modules/police/police.controller.js
@@ -1,21 +1,32 @@
 import { postModel } from "../../../DB/models/post.model.js";
+import { userModel } from "../../../DB/models/user.model.js";
 import { asyncHandler } from "../../utils/errorHandling.js";
 
 export const updateLocation = asyncHandler(async (req, res, next) => {
-    const { postId  } = req.params;
-  
-    const post = await postModel.findById(postId).populate([{
-      path:"imageId",
-      select:"images.url"
+  const { postId } = req.params;
+
+  const post = await postModel.findById(postId).populate([
+    {
+      path: "imageId",
+      select: "images.url",
     },
     {
-      path:"createdBy",
-      select:"name profileImage.url"
-    }
-]);
-    if (!post) return next(new Error("Post Not Exist"));
-  
-    post.address = req.body.address;
-    await post.save();
-    return res.json({ success: true, results: post });
-  });
+      path: "createdBy",
+      select: "name profileImage.url",
+    },
+  ]);
+  if (!post) return next(new Error("Post Not Exist"));
+
+  post.address = req.body.address;
+  await post.save();
+  return res.json({ success: true, results: post });
+});
+
+export const getAllDar = asyncHandler(async (req, res, next) => {
+  const users = await userModel.find({ role: "dar" }).select('name')
+  if (!users) {
+    return res.json({ message: "Dars not found !" });
+  }
+
+  return res.json({ success: true, results:users});
+});
--- src/Modules/police/police.router.js
+++ src/Modules/police/police.router.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { isAuthenticated } from "../../Middleware/authentication.middleware.js";
 import { isAuthorized } from "../../Middleware/authorizaion.middleware.js";
 import { allPosts } from "../posts/post.controller.js";
-import { updateLocation } from "./police.controller.js";
+import { getAllDar, updateLocation } from "./police.controller.js";
 import { updateLocationSchema } from "./police.validation.js";
 import { isValid } from "../../Middleware/validation.middleware.js";
 
@@ -14,4 +14,5 @@ router.get("/allpost" , isAuthenticated , isAuthorized("police") , allPosts)
 
 router.post("/changelocation/:postId",isAuthenticated,isAuthorized("police") ,isValid(updateLocationSchema) ,updateLocation)
 
+router.get("/alldar",isAuthenticated,isAuthorized("police"),getAllDar)
 export default router;
